56b28ea0d03016e575e6639e248f7d5cc2ef2268
David Sadler <davidwallacesadler@gmail.com>
2022-12-13 21:27:57 -0700

    Giving up on day 13... parsing this is just taking far too long

--- AdventOfCode22.playground/Pages/Day 13.xcplaygroundpage/Contents.swift
+++ AdventOfCode22.playground/Pages/Day 13.xcplaygroundpage/Contents.swift
@@ -13,33 +13,109 @@ let packetPairs = inputData.components(separatedBy: "\n\n")
 
 struct Packet {
     
-    let dataList: [DataList]
+    let dataLists: [DataList]
     
     struct DataList {
         let depth: Int
+        let isASingleValue: Bool
         let values: [Int]
+        
+        init(depth: Int, value: Int) {
+            self.depth = depth
+            self.values = [value]
+            self.isASingleValue = true
+        }
+        
+        init(depth: Int, values: [Int]) {
+            self.depth = depth
+            self.values = values
+            self.isASingleValue = false
+        }
     }
     
-        init?(textRepresentation: String) {
-            guard !textRepresentation.isEmpty else { return nil }
-            
-            var currentString = ""
-            var awaitingNumber: Bool = false
-            
-            for char in textRepresentation {
-                if char == "[" && !awaitingNumber {
-                    currentString = "["
-                }
-                if char.isNumber {
-                    if awaitingNumber {
-                        currentString.app
+    init?(textRepresentation: String) {
+        guard !textRepresentation.isEmpty else { return nil }
+        
+        var datas: [DataList] = []
+        var currentDepth = 0
+        var currentListString = ""
+        
+        for char in textRepresentation {
+            print("Looking at \(char)")
+            switch char {
+            case "[":
+                if !currentListString.isEmpty {
+                    print("Have a build string... need to add it: \(currentListString)")
+                    let nums = currentListString.components(separatedBy: ",").compactMap({ Int($0) })
+                    print("nums \(nums)")
+                    if nums.count > 1 {
+                        print("Adding nums")
+                        datas.append(DataList(depth: currentDepth, values: nums))
+                    } else {
+                        print("Adding single num")
+                        if currentListString.last!.isPunctuation {
+                            datas.append(DataList(depth: currentDepth, values: nums))
+                        } else {
+                            datas.append(DataList(depth: currentDepth, value: nums[0]))
+                        }
                     }
-                    currentString = "\(char)"
                 }
+                print("Increasing depth to \(currentDepth + 1)")
+                currentDepth += 1
+                currentListString = ""
+                continue
+            case "]":
+                // End of list
+                print("Got an end brace at depth \(currentDepth)")
+                if currentDepth > 1 {
+                    if currentListString.isEmpty && currentListString != "]" {
+                        print("No list string - adding empty array")
+                        datas.append(DataList(depth: currentDepth, values: []))
+                    } else {
+                        let nums = currentListString.components(separatedBy: ",").compactMap({ Int($0) })
+                        print("Adding list \(nums)")
+                        if nums.count > 1 {
+                            print("Adding nums")
+                            datas.append(DataList(depth: currentDepth, values: nums))
+                        } else {
+                            print("Adding single num")
+                            datas.append(DataList(depth: currentDepth, value: nums[0]))
+                        }
+                    }
+                }
+                if currentDepth == 1 && !currentListString.isEmpty {
+                    let nums = currentListString.components(separatedBy: ",").compactMap({ Int($0) })
+                    print("Adding list \(nums)")
+                    if nums.count > 1 {
+                        print("Adding nums")
+                        datas.append(DataList(depth: currentDepth, values: nums))
+                    } else {
+                        print("Adding single num")
+                        datas.append(DataList(depth: currentDepth, value: nums[0]))
+                    }
+                }
+
                 
+                currentDepth -= 1
+                currentListString = "]"
+                continue
+            case ",":
+                if currentListString.isEmpty {
+                    continue
+                }
+                currentListString.append(char)
+            default:
+                print("Num char - adding to build string...")
+                currentListString.append(char)
+                print("Build string is now \(currentListString)")
             }
-            
-            
-            
         }
+        self.dataLists = datas
+    }
+}
+
+let packet = Packet(textRepresentation: "[1,[2,[3,[4,[5,6,7]]]],8,9]")!
+
+for data in packet.dataLists {
+    print("Data: \(data.values) at depth \(data.depth) is a single value \(data.isASingleValue ? "yes" : "no")")
 }
